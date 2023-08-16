397c32b966def2e00f1a5ebcbaf227aac46543a5
Cesar Acero <fabi-1530@hotmail.com>
2023-08-15 19:47:06 -0500

    creacion de clases e instanciamiento

--- practicaPoo/practicaPoo/Program.cs
+++ practicaPoo/practicaPoo/Program.cs
@@ -10,6 +10,18 @@ namespace practicaPoo
     {
         static void Main(string[] args)
         {
+            Circulo miCirculo;                      //Creacion de objeto de tipo circulo 
+            miCirculo = new Circulo();              //Instanciamos una clase
+            
+            Console.WriteLine(miCirculo.calculoArea(5));
+        }
+    }
+    class Circulo
+    {
+        public double pi = 3.1416;     //declaramos una constante ( propiedad de la circunferencia. Campo de clase )
+        public double calculoArea (int radio)  //metodo de clase. ¿Qué pueden hacer los objetos de tipo circulo?
+        {
+            return pi * radio*radio;
         }
     }
 }
